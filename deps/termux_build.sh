895368a49efae1ac4e23258bbcb2edae3de50674
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-21 16:44:33 +0000

    Update termux_build.sh

--- deps/termux_build.sh
+++ deps/termux_build.sh
@@ -10,5 +10,5 @@ git clone https://github.com/termux/termux-packages.git
 mkdir -p ./termux-packages/x11-repo/gw
 cp ./gw/deps/build.sh ./termux-packages/x11-repo/gw
 ./termux-packages/scripts/./setup-termux.sh
-./termux-packages/build-package.sh -I gw
-#rm -rf termux-packages
+./termux-packages/build-package.sh -I ~/termux-packages/x11-repo/gw
+rm -rf termux-packages
